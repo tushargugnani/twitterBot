98962141a7f713c2bf9550660c631627d16d4e6d
Tushar Gugnani <tushar.gugnani@gmail.com>
2020-02-13 18:52:28 +0530

    Changes

--- app/Commands/PostTweet.php
+++ app/Commands/PostTweet.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
@@ -61,22 +62,32 @@ class PostTweet extends Command
      */
     public function postTweetOnTwitter($tweet){
         $this->browse(function ($browser) use($tweet)  {
+
             $browser->visit('https://twitter.com/login')
-                    ->pause(2000);
+                    ->pause(4000);
+
 
             $browser->type('session[username_or_email]', env('twitter_username'))
                     ->type('session[password]', env('twitter_password'))
                     ->pause(1000)
                     ->click('div[role="button"]')
-                    ->pause(4000)
-                    ->assertSee('Home')
+                    ->pause(4000);
+
+                if($browser->element('#challenge_response') != null){
+                        $browser->value('#challenge_response', env('challenge_response'))
+                                ->click('#email_challenge_submit')
+                                ->pause(3000);
+                }
+
+                //Post Tweet
+                $browser->assertSee('Home')
                     ->pause(2000)
                     ->click('.public-DraftStyleDefault-block')
                     ->keys(".public-DraftStyleDefault-block", $tweet)
                     ->pause(4000)
                     ->click('div[data-testid="tweetButtonInline"]')
                     ->pause(3000);
-                    
+         
         });
     }
 
